3bf3957fd2f91e3887842ac6aa2a6fd3bf4ad929
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2021-12-30 15:55:06 +0200

    add initial project from tutorial

---
+++ src/components/event-list.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export const EventList = ({ events }) => {
+  return (
+    <>
+      <h1>Upcoming events</h1>
+      <ul>
+        {events.map((event) => (
+          <li key={event.id}>
+            <strong>
+              <Link to={event.slug}>{event.name}</Link>
+            </strong>
+            <br />
+            {new Date(event.startDate).toLocaleDateString("en-US", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })}
+            in {event.location}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default EventList;
---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Layout = ({ children }) => {
+  return (
+    <div>
+      <h1>Gatsby Events Theme</h1>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
