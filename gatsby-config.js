7de4ffb6d63a3e3530d50853147ab385b7409799
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2021-12-30 17:31:56 +0200

    add page creator plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = ({ contentPath = "data", basePath = "/" }) => ({
       },
     },
     {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
       resolve: "gatsby-transformer-yaml",
       options: {
         typeName: "Event",
