fd67e6949bf1398b8161497ed2e0a46a679177c6
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2022-01-16 16:58:07 +0200

    add default avatar image

--- src/components/common/avatar.tsx
+++ src/components/common/avatar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
 
 import { Box } from "@twilio-paste/box";
 import { Width } from "@twilio-paste/style-props";
@@ -10,11 +10,15 @@ type Props = { width?: Width };
 
 export const Avatar = ({ width }: Props) => {
   const { siteMetadata, avatar } = useSiteMetadata();
-  const image = getImage(avatar);
+  const avatarImage = getImage(avatar);
 
   return (
     <Box width={width}>
-      <GatsbyImage image={image} alt={siteMetadata.author.name} style={{ borderRadius: "50%" }} />
+      {avatarImage ? (
+        <GatsbyImage image={avatarImage} alt={siteMetadata.author.name} style={{ borderRadius: "50%" }} />
+      ) : (
+        <StaticImage src="../../../static/avatar.png" alt={siteMetadata.author.name} width={200} style={{ borderRadius: "50%" }} />
+      )}
     </Box>
   );
 };
